49b5a9033f13b10dc8274b730fe0f7c9b7eb94e5
AngieDiaz25 <diazs.angie@gmail.com>
2025-11-19 11:54:55 +0100

    Initial commit - MoodBot API

---
+++ preprocessing.py
@@ -0,0 +1,60 @@
+"""
+Preprocesamiento de texto para MoodBot
+"""
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except:
+    nltk.download('punkt', quiet=True)
+try:
+    nltk.data.find('corpora/stopwords')
+except:
+    nltk.download('stopwords', quiet=True)
+try:
+    nltk.data.find('corpora/wordnet')
+except:
+    nltk.download('wordnet', quiet=True)
+try:
+    nltk.data.find('corpora/omw-1.4')
+except:
+    nltk.download('omw-1.4', quiet=True)
+
+class TextPreprocessor:
+    def __init__(self):
+        self.lemmatizer = WordNetLemmatizer()
+        self.stop_words = set(stopwords.words('english'))
+    
+    def clean_text(self, text):
+        if not isinstance(text, str):
+            return ""
+        text = text.lower()
+        text = re.sub(r'http\S+|www\S+|https\S+', '', text)
+        text = re.sub(r'@\w+', '', text)
+        text = re.sub(r'#\w+', '', text)
+        text = re.sub(r'[^a-zA-Z\s]', '', text)
+        text = re.sub(r'\s+', ' ', text).strip()
+        return text
+    
+    def tokenize_and_lemmatize(self, text):
+        if not text:
+            return ""
+        tokens = word_tokenize(text)
+        processed = [
+            self.lemmatizer.lemmatize(token)
+            for token in tokens
+            if token not in self.stop_words and len(token) > 2
+        ]
+        return ' '.join(processed)
+    
+    def preprocess(self, text):
+        cleaned = self.clean_text(text)
+        processed = self.tokenize_and_lemmatize(cleaned)
+        return processed
+
+
